125a3dbd9f9feba371774efa950b628976289f12
gabriel-v <gabriel.vijiala@gmail.com>
2015-05-13 17:31:51 +0300

    Finalizeaza detaliile.

--- detalii.php
+++ detalii.php
@@ -20,6 +20,23 @@ require_once("php/elemente.php");
         <?php
         echo_navbar('detalii');
         ?>
+        
+        
+        <div class="container">
+            <div class="jumbotron">
+                <h1>
+                    Sistem de mesagerie
+                </h1>
+                <p>
+                    <b>Chatbox</b> este proiectul meu pentru Atestatul la Informatică, 
+                    din clasa a XII-a. 
+                </p>
+                <p>
+                    Documentația pentru proiect poate fi găsită 
+                    <a target='blank' href='atestat-chatbox-12i-cnmv-2015.pdf'>aici</a>.
+                </p>
+            </div>
+        </div>
 
     </body>
 </html>
